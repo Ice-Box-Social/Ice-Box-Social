5ad14e78abb14cd1892ca25ffcae8273a852f8c6
Tameflame <azam3754@gmail.com>
2023-03-18 22:32:10 +0800

    work on notes

--- src/Components/Notes/Note.js
+++ src/Components/Notes/Note.js
@@ -1,14 +1,20 @@
 // import { MdDeleteForever } from 'react-icons/md';
 import './Note.css';
 
-const Note = ({ id, text, stickerIndex }) => {
+const Note = ({ id, text, stickerIndex, noteColor}) => {
+
+	var stickerNames = ["Heart","OK", "Sun", "Yes", "Star"];
+
 	return (
-		<div className='note'>
+		<div className='ice-image'>
+		<img src= './img/notes/Snowsmall2.svg' className='snowsmall'></img>
+		<div className='note' style={{backgroundColor: noteColor}}>
 			<span>{text}</span>
 			<div className='note-footer'>
 				<small>{id}</small>
-				<img src={"./img/Star.svg"} alt="Sticker" />
+				<img src={`./img/stickers/${stickerNames[stickerIndex]}.svg`} alt="Sticker" />
 			</div>
+		</div>
 		</div>
 	);
 };
