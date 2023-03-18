472e5197eb81b40eddcd16fa7abe20be023c8351
Tameflame <azam3754@gmail.com>
2023-03-18 21:47:50 +0800

    implement notes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import NotesList from './Components/Notes/NotesList';
+
 
 const App = () => {
+
+  // The list of notes to display
+  // TODO: Figure out how to fetch the notes list from database
+  const [notes, setNotes] = useState([
+    {
+      id: 'testNoteId',
+      text: 'Test Note',
+      date: '16/03/2023',
+    }
+  ])
+
+  // TODO: Implement adding a new note
+  const addNote = (text) => {
+
+		// const date = new Date();
+		// const newNote = {
+		// 	id: nanoid(),
+		// 	text: text,
+		// 	date: date.toLocaleDateString(),
+		// };
+		// const newNotes = [...notes, newNote];
+		// setNotes(newNotes);
+	};
+
+  // TODO: Implement deleting a note? Nah probably note necessary
+  const deleteNote = (id) => {
+		// const newNotes = notes.filter((note) => note.id !== id);
+		// setNotes(newNotes);
+	};
+
   return (
     <div className="App">
+        <NotesList 
+            notes={notes}
+            handleAddNote={addNote}
+            handleDeleteNote={deleteNote}>
+        </NotesList>
     </div>
   );
 }
