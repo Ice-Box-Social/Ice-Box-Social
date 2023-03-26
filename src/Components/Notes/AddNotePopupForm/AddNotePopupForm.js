803fbfcff9bb4710754aa84b66f820f107f79932
Tameflame <azam3754@gmail.com>
2023-03-26 21:49:30 +0800

    try polybase

--- src/Components/Notes/AddNotePopupForm/AddNotePopupForm.js
+++ src/Components/Notes/AddNotePopupForm/AddNotePopupForm.js
@@ -8,6 +8,9 @@ import { NFTStorage, Blob } from 'nft.storage'
 import { message } from 'antd';
 import IceBoxContract from '../../../Contract/IceBoxMessage.json';
 import { ethers } from 'ethers';
+import { secp256k1 } from '@polybase/util';
+import axios from 'axios';
+import { ethPersonalSign } from '@polybase/eth'
 
 export const AddNotePopupForm = ({closeModal}) => {
 
@@ -27,6 +30,7 @@ export const AddNotePopupForm = ({closeModal}) => {
       setLoading(true);
       const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
       const provider = new ethers.providers.Web3Provider(window.ethereum);
+      addToPolybase();
       await provider.send("eth_requestAccounts", []);
       const signer = provider.getSigner();
       console.log("Account is: " + accounts[0]);
@@ -43,8 +47,7 @@ export const AddNotePopupForm = ({closeModal}) => {
       const cid = await nftstorage.storeBlob(data);
       console.log(cid);
       const response = await Contract.safeMint(accounts[0],cid);
-      console.log(response);
-      // await addToPolybase();
+      // console.log(response);
       message.success("Notes Added Successfully!")
       setLoading(false);
       closeModal();
@@ -54,13 +57,65 @@ export const AddNotePopupForm = ({closeModal}) => {
     }
   }
 
+  function createSig (timestamp, body) {
+    try {
+
+    // let privateKey = process.env.POLYBASE_PRIVATE_KEY;
+    var privateKey = 0x199e2434814e840f9e73f2dc4683f59e360d39115baa6d2827be70ffdba9e8a9n;
+
+    const str_to_sign = `${timestamp}.${JSON.stringify(body)}`;
+    // console.log(`Stringified String is: ${str_to_sign}`)
+    // let privateKey = secp256k1.generatePrivateKey();
+    // return secp256k1.sign(privateKey, str_to_sign);
+    return ethPersonalSign(privateKey, str_to_sign);
+    } catch (error) {
+      console.log(`Error creating sig: ${error}`);
+    }
+  }
+
+
   const addToPolybase = async (event) => {
     try {
+      // let private_key = process.env.REACT_APP_NFT_STORAGE_KEY;
+
+      var body = {
+        args : [
+          Date.now().toString(),
+          addNoteText.toString(),
+          selectedColor.toString(),
+          selectedSticker.toString()
+        ]
+      }
+
+
+      // var url = "pk%2F0x593e1594134c988cec1d20de3050e6a001c843e88e54f1e94041fb21623946771da020a4f016e7074e6b6cb0ecb933cc285565bdd791aa67b462b72a359360c1%2Ficeboxsocial/collections/User"
+      // var url = "https://testnet.polybase.xyz/v0/collections/pk%2F0x593e1594134c988cec1d20de3050e6a001c843e88e54f1e94041fb21623946771da020a4f016e7074e6b6cb0ecb933cc285565bdd791aa67b462b72a359360c1%2Ficeboxsocial/User"
+      var url = "https://testnet.polybase.xyz/v0/collections/pk%2F0x593e1594134c988cec1d20de3050e6a001c843e88e54f1e94041fb21623946771da020a4f016e7074e6b6cb0ecb933cc285565bdd791aa67b462b72a359360c1%2Ficeboxsocial%2FUser/records"
+
+      var version = 0;
+      var timestamp = Date.now();
+      var hash = 'eth-personal-sign';
+      let privateKey = secp256k1.generatePrivateKey();
+
+      var sig = createSig(timestamp, body, privateKey)
+
+      var headers = {
+        // 'Accept' : 'application/json',
+        'X-Polybase-Signature' : `v=0,t=${timestamp},h=eth-personal-sign,sig=${sig}`,
+      };
+      // example header 
+      // X-Polybase-Signature: v=0,t=1671884992,h=eth-personal-sign,sig=0x288db6271d92253ae19983a8e5110f1bc1bef1911210127ccbf657d85428ba9917aa9457f0f8e7f300a36b106525d3a3471e63ae265af4898742040a377e1da11c
+      await axios.post(url, body, {
+        headers: headers,
+      });
+    
       
     } catch (error) {
       setLoading(false);
-      console.log(error);
+      console.log(`Error creating record on PolyBase: ${error}`);
     }
+
+  
   }
 
   return (
